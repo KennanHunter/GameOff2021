11d20bd0ebcdac5d7078bb819676bc09d9b1c1b4
Travh98 <travishunter33@gmail.com>
2021-11-11 14:27:06 -0500

    Spider poison spikes now push away from other spikes, covering larger distance

--- Assets/Scripts/SpiderPoisonBall.cs
+++ Assets/Scripts/SpiderPoisonBall.cs
@@ -17,35 +17,43 @@ public class SpiderPoisonBall : MonoBehaviour
     [SerializeField]
     public float spikeSightRange = 5f;
 
+    private float spikePushSightDistance = 5.0f;
     private float attackTimer = 0f;
 
     private void FixedUpdate()
     {
         // Rotate spikes to be scary
-        transform.Rotate(0, 0, attackDamage * 90 * Time.deltaTime);
+        transform.Rotate(0, 0, attackDamage * 360 * Time.deltaTime);
 
         // Push away from parent (Spider)
         Vector2 outDirection = (transform.position - transform.parent.position);
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.AddForce(outDirection * Time.deltaTime * attackDamage, ForceMode2D.Force);
 
-        // Push spikes away from other spikes
-        Collider2D[] otherEnemyUnits = Physics2D.OverlapCircleAll(gameObject.transform.position, attackRange, enemyLayers);
-        // Find closest otherEnemyUnit and push away from it
-        float distanceFromOthers = 20f;
-        float shortestDistance = 20f;
-        Collider2D closestCollider = null;
+        // Push spikes away from other spikes and enemies
+        // Scan for other enemy units within spikeSightDistance
+        Collider2D[] otherEnemyUnits = Physics2D.OverlapCircleAll(gameObject.transform.position, spikeSightRange, enemyLayers);
+        // Set initial values, anything close should be within spikePushSightDistance
+        float distanceFromOthers, shortestDistance = spikePushSightDistance;
+        Collider2D closestCollider = GetComponent<Collider2D>();
+
         foreach (Collider2D enemy in otherEnemyUnits)
         {
-            distanceFromOthers = (transform.position - enemy.GetComponentInParent<Transform>().position).magnitude;
-            if(distanceFromOthers < shortestDistance)
+            // If we're considering ourselves, ignore
+            if (enemy.transform.position == transform.position)
+            {
+                continue;
+            }
+
+            distanceFromOthers = (transform.position - enemy.GetComponent<Transform>().position).magnitude;
+            if (distanceFromOthers < shortestDistance)
             {
                 shortestDistance = distanceFromOthers;
                 closestCollider = enemy;
-                Debug.Log("Closest Enemy: " + closestCollider);
             }
         }
-        Vector2 pushDirection = transform.position - closestCollider.GetComponentInParent<Transform>().position;
+        Vector2 pushDirection = (transform.position - closestCollider.GetComponent<Transform>().position).normalized;
+        rb.AddForce(pushDirection * Time.deltaTime * attackDamage * 500, ForceMode2D.Force);
     }
 
     // Update is called once per frame
